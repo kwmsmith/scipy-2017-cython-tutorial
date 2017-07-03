234095c882197a9738151497cc6d99a8e918e8d9
Kurt Smith <kusmith@homeaway.com>
2017-07-03 17:30:00 -0500

    peg docker image version, update launch script.

--- launch-container.sh
+++ launch-container.sh
@@ -4,6 +4,6 @@ docker run \
     -it \
     --rm \
     -p 8888:8888 \
-    -v `pwd`:/home/jovyan/work \
+    -v `pwd`:/home/jovyan/ \
     --name cython-tutorial \
-    jupyter/scipy-notebook
+    jupyter/scipy-notebook:8e15d329f1e9
